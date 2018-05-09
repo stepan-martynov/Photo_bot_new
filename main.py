bc2c3f700f7a29858991ee75ae51aff5d63e0240
Stepan Martynov <smartd88@gmail.com>
2018-05-10 00:25:52 +0300

    flask

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 # -*- coding: utf-8 -*-
 
-import telebot
+from flask import Flask
 
-bot = telebot.TeleBot('366514649:AAEL4gxETw62Rw1fdDoHuVB8MlVQpUeNtJY')
+app = Flask(__name__)
 
 
-@bot.message_handler(func=lambda message: True, content_types=['text'])
-def echo_message(message):
-    bot.reply_to(message.chat.id, message.text)
+@app.route('/')
+def index():
+    return 'ok'
+
+
+# import telebot
+
+# bot = telebot.TeleBot('366514649:AAEL4gxETw62Rw1fdDoHuVB8MlVQpUeNtJY')
+#
+#
+# @bot.message_handler(func=lambda message: True, content_types=['text'])
+# def echo_message(message):
+#     bot.reply_to(message.chat.id, message.text)
+#
 
 
 if __name__ == '__main__':
-        bot.polling(none_stop=True)
+    app.run()
+    # bot.polling(none_stop=True)
